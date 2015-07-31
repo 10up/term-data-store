eed6d2992a867daf405c1f70d9a5d5374849170d
John P Bloch <johnpbloch@gmail.com>
2015-07-31 00:46:26 -0500

    Add a test for invalid taxonomy

--- tests/unit/AddRelationshipTest.php
+++ tests/unit/AddRelationshipTest.php
@@ -23,4 +23,20 @@ class AddRelationshipTest extends TestCase {
 		add_relationship( $post_type, 'taxonomy' );
 	}
 
+	/**
+	 * @expectedException \TDS\Invalid_Input_Exception
+	 * @expectedExceptionMessage TDS\add_relationship() invalid taxonomy input.
+	 */
+	public function test_add_relationship_invalid_taxonomy() {
+		$post_type = 'post' . rand( 0, 9 );
+		$taxonomy  = 'post_tag' . rand( 0, 9 );
+		\WP_Mock::userFunction( 'get_post_type_object', array( 'return' => (object) array( 'name' => $post_type ) ) );
+		\WP_Mock::userFunction( 'get_taxonomy', array(
+			'times'  => 1,
+			'args'   => array( $taxonomy ),
+			'return' => null,
+		) );
+		add_relationship( $post_type, $taxonomy );
+	}
+
 }
