9bf89701638704e8953bb90329181abf928f95ba
John P Bloch <johnpbloch@gmail.com>
2015-07-30 23:54:47 -0500

    First test class

---
+++ tests/unit/GetRelationshipTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TDS;
+
+use TDS\Tools\TestCase;
+
+class GetRelationshipTest extends TestCase {
+
+	public function test_get_relationship() {
+		$this->markTestIncomplete();
+	}
+
+}
