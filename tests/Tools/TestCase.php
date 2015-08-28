572d14154bd67bc6153c3c663707a0cf5b4c2810
John P Bloch <johnpbloch@gmail.com>
2015-08-28 10:19:43 -0500

    Add a Mock class for WP_Query

--- tests/Tools/TestCase.php
+++ tests/Tools/TestCase.php
@@ -6,4 +6,18 @@ use WP_Mock\Tools\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase {
 
+	public function setUp() {
+		\WP_Query::$__instances = array();
+		\WP_Query::$__posts     = array();
+		\WP_Query::$__mocks     = array();
+		parent::setUp();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		\WP_Query::$__instances = array();
+		\WP_Query::$__posts     = array();
+		\WP_Query::$__mocks     = array();
+	}
+
 }
