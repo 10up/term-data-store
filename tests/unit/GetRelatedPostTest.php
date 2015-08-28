d65f360b02c1dbca673c33c90ddc6d8423266763
John P Bloch <johnpbloch@gmail.com>
2015-08-28 10:23:01 -0500

    Add a test for expected behavior when there's no relationship for the taxonomy

--- tests/unit/GetRelatedPostTest.php
+++ tests/unit/GetRelatedPostTest.php
@@ -2,8 +2,41 @@
 
 namespace TDS;
 
+use Mockery;
 use TDS\Tools\TestCase;
+use WP_Mock;
 
 class GetRelatedPostTest extends TestCase {
+
+	public function test_get_related_post_noop_when_empty_taxonomy() {
+		$this->assertNull( get_related_post( 1, '' ) );
+	}
+
+	public function test_get_related_post_noop_when_term_not_object_or_int() {
+		WP_Mock::wpFunction( 'is_wp_error', array( 'return' => false ) );
+		$this->assertNull( get_related_post( 'foobar', 'category' ) );
+	}
+
+	public function test_get_related_post_noop_when_term_is_wp_error() {
+		WP_Mock::wpFunction( 'get_term', array(
+			'times'  => 1,
+			'args'   => array( 1, 'category' ),
+			'return' => Mockery::mock( 'WP_Error' ),
+		) );
+		WP_Mock::wpFunction( 'is_wp_error', array( 'return' => true ) );
+		$this->assertNull( get_related_post( 1, 'category' ) );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function test_get_related_post_noop_when_relationship_not_set() {
+		$term = (object) array(
+			'term_id'  => rand( 1, 9 ),
+			'taxonomy' => 'category',
+		);
+		WP_Mock::wpFunction( 'is_wp_error', array( 'return' => false ) );
+		$this->assertNull( get_related_post( $term, $term->taxonomy ) );
+	}
 
 }
