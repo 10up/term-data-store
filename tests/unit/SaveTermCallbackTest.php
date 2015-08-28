ffecfa6e7813c189127c6774d7477f300193a04c
John P Bloch <johnpbloch@gmail.com>
2015-08-28 00:46:14 -0500

    Add a test to verify that the bound post gets created when the term has no posts yet

--- tests/unit/SaveTermCallbackTest.php
+++ tests/unit/SaveTermCallbackTest.php
@@ -104,4 +104,47 @@ class SaveTermCallbackTest extends TestCase {
 		$this->assertConditionsMet();
 	}
 
+	public function test_callback_when_term_has_no_objects() {
+		$term_id   = rand( 1, 9 );
+		$post_type = 'post';
+		$taxonomy  = 'category';
+		$term      = (object) array(
+			'term_id'  => $term_id,
+			'name'     => 'Foo Bar',
+			'slug'     => 'foo-bar',
+			'taxonomy' => $taxonomy,
+		);
+		WP_Mock::wpFunction( 'get_objects_in_term', array(
+			'times'  => 1,
+			'args'   => array( $term_id, $taxonomy ),
+			'return' => array(),
+		) );
+		WP_Mock::wpFunction( 'get_post_type', array( 'times'  => 0 ) );
+		WP_Mock::wpFunction( 'get_term', array(
+			'times'  => 1,
+			'args'   => array( $term_id, $taxonomy ),
+			'return' => $term,
+		) );
+		$post_id = rand( 30, 39 );
+		WP_Mock::wpFunction( 'wp_insert_post', array(
+			'times'  => 1,
+			'args'   => array(
+				array(
+					'post_type'   => $post_type,
+					'post_title'  => $term->name,
+					'post_name'   => $term->slug,
+					'post_status' => 'publish',
+				)
+			),
+			'return' => $post_id,
+		) );
+		WP_Mock::wpFunction( 'wp_set_object_terms', array(
+			'times' => 1,
+			'args'  => array( $post_id, $term_id, $taxonomy ),
+		) );
+
+		call_user_func( get_save_term_hook( $post_type, $taxonomy ), $term_id );
+		$this->assertConditionsMet();
+	}
+
 }
