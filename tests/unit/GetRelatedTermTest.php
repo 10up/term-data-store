da531f2d689ebd58853e69beb851bf4d80d86ea0
John P Bloch <johnpbloch@gmail.com>
2015-08-31 21:34:56 -0500

    Add a class to test get_related_term

---
+++ tests/unit/GetRelatedTermTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TDS;
+
+use TDS\Tools\TestCase;
+
+class GetRelatedTermTest extends TestCase {
+
+	public function test_get_related_term_noop_when_no_post() {
+		$this->markTestIncomplete();
+	}
+
+}
