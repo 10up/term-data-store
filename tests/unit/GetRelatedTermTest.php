3394fbc75f2a24b7b4b541c94ff0c9b63c542139
John P Bloch <johnpbloch@gmail.com>
2015-08-31 21:35:24 -0500

    Fill in first test for get_related_term

--- tests/unit/GetRelatedTermTest.php
+++ tests/unit/GetRelatedTermTest.php
@@ -3,11 +3,13 @@
 namespace TDS;
 
 use TDS\Tools\TestCase;
+use WP_Mock;
 
 class GetRelatedTermTest extends TestCase {
 
 	public function test_get_related_term_noop_when_no_post() {
-		$this->markTestIncomplete();
+		WP_Mock::wpFunction( 'get_post', array( 'return' => null ) );
+		$this->assertNull( get_related_term( 1 ) );
 	}
 
 }
